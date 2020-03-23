7aeb3e85dd33605e9543943b9f839107d4f48b08
princechetu1 <chetanpatil712@gmail.com>
2020-03-23 16:45:52 +0530

    adding build folder

---
+++ corona-info/build/precache-manifest.d5a48923469ca001947272cd457ac3aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6e33e20d6d09328921406d20fd344a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10188ec7f2fef1b99483",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "28ae1ac18e4b9b8fdb28",
+    "url": "/static/css/main.a2d1e9be.chunk.css"
+  },
+  {
+    "revision": "10188ec7f2fef1b99483",
+    "url": "/static/js/2.f3b2f30c.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.f3b2f30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ae1ac18e4b9b8fdb28",
+    "url": "/static/js/main.3967ca65.chunk.js"
+  },
+  {
+    "revision": "05c1c72ed5aa10ac58cd",
+    "url": "/static/js/runtime-main.b6b8836b.js"
+  },
+  {
+    "revision": "45a106f17aea582f822b501b9e372beb",
+    "url": "/static/media/applicationBackground2.45a106f1.jpg"
+  }
+]);
